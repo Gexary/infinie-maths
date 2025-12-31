765735ff23b3dd4b9452082610368c3ffb56b4de
Gexary <gexary.gex@gmail.com>
2026-01-01 00:48:28 +0100

    rename

--- app/(Root)/layout.tsx
+++ app/(Root)/layout.tsx
@@ -4,7 +4,7 @@ import { AppProvider } from "@/contexts/app-context";
 import { getGradesCollection } from "@/db/models/grades-collection";
 
 export default async function PageLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  const json = await getGradesCollection();
+  const gradesCollection = await getGradesCollection();
 
-  return <AppProvider gradesCollection={json}>{children}</AppProvider>;
+  return <AppProvider gradesCollection={gradesCollection}>{children}</AppProvider>;
 }
